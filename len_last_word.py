5002981ab89ade796aa43a54f07fce623d1e8da3
Hari <Prasad.k4u@gmail.com>
2026-06-03 14:47:09 -0500

    test

--- len_last_word.py
+++ len_last_word.py
@@ -1,5 +1,5 @@
 """ find lenght of last word in a sentence"""
 
-my_str=" hello my hobbies are playing @CAricket "
+my_str=" hello my hobbies are playing @CAricket"
 
 print(len(my_str.strip(" ").split(" ")[-1]))
